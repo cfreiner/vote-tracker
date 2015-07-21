da422e499d47c765e7ea715596bce4e4ccd8a36c
Carl Reiner <creiner1@gmail.com>
2015-07-21 11:54:58 -0700

    Added the Chart.js library and a bar chart.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-var Photo = function(url){
+var Photo = function(url) {
   this.url = url;
 }
 
-var Tracker = function(){
+var Tracker = function() {
   this.photos = [];
 }
 
@@ -13,7 +13,7 @@ Tracker.prototype.generateRandom = function(max) {
 
 //Picks a random number between zero and the number of photos
 //Deprecated function. To be removed soon.
-Tracker.prototype.pickRandomPhoto = function(){
+Tracker.prototype.pickRandomPhoto = function() {
   return this.generateRandom(this.photos.length);
 };
 
@@ -31,7 +31,7 @@ Tracker.prototype.addPhoto = function(photo) {
 };
 
 //Display two random photos
-Tracker.prototype.renderComparison = function(){
+Tracker.prototype.renderComparison = function() {
   var photo1 = this.renderRandomPhoto();
   var photo2 = this.renderRandomPhoto();
   while(photo1 == photo2){
@@ -44,12 +44,34 @@ Tracker.prototype.renderComparison = function(){
   highlight();
 };
 
+//Render the chart comparing votes for the two pics
+var theChart = null;
+Tracker.prototype.renderChart = function() {
+  var votes1 = this.getVotes($("#photo1 img").attr("src"));
+  var votes2 = this.getVotes($("#photo2 img").attr("src"));
+  var chartData = {
+    labels: ["Cat 1", "Cat 2"],
+    datasets: [
+      {
+        fillColor: "orange",
+        strokeColor: "black",
+        data: [votes1, votes2]
+      }
+    ]
+  };
+  var votes = document.getElementById("chart").getContext("2d");
+  if(theChart) {
+    console.log("destroying");
+    theChart.destroy();
+  }
+  theChart = new Chart(votes).Bar(chartData);
+};
+
 //Gets the index of a photo in the array with a particular url.
 //Returns false if the photo isn't there.
 Tracker.prototype.findPhotoInArray = function(url) {
-  console.log(url);
-  for(var i = 0; i < this.photos.length; i++){
-    if(this.photos[i][0].url == url){
+  for(var i = 0; i < this.photos.length; i++) {
+    if(this.photos[i][0].url == url) {
       return i;
     }
   }
@@ -57,32 +79,88 @@ Tracker.prototype.findPhotoInArray = function(url) {
 };
 
 //Add a vote for a photo
-Tracker.prototype.incrementVote = function(photo){
+Tracker.prototype.incrementVote = function(photo) {
   var index = this.findPhotoInArray(photo);
   this.photos[index][1]++;
 };
 
+//Remove a vote for a photo
+Tracker.prototype.decrementVote = function(photo) {
+  var index = this.findPhotoInArray(photo);
+  this.photos[index][1]--;
+};
+
+//Get the number of votes that a photo currently has
+Tracker.prototype.getVotes = function(photo) {
+  var index = this.findPhotoInArray(photo);
+  return this.photos[index][1];
+}
+
 //Event listener for the button click
 //May be moved to the Tracker constructor later
-$(document).ready(function(){
-  $('button').click(function(){
-    testTracker.incrementVote($('.highlight').attr('src'));
+$(document).ready(function() {
+  $('button').click(function() {
     $('button').css({'visibility':'hidden'});
     testTracker.renderComparison();
+    testTracker.renderChart();
   });
 });
 
 //Event listener for highlighting the user choice.
 //Has to be called every time new images are drawn to reattach the listener.
-var highlight = function(){
-  $(document).ready(function(){
-    $('img').click(function(){
-    $('button').css({'visibility':'visible'});
-    $('.highlight').removeClass('highlight');
-    $(this).toggleClass('highlight');
+// var highlight = function() {
+//   $(document).ready(function() {
+//     $('img').click(function(e) {
+//       $('button').css({'visibility':'visible'});
+//       $('.highlight').removeClass('highlight');
+//       $(this).toggleClass('highlight');
+//     });
+//   });
+// };
+
+var highlight = function() {
+  $(document).ready(function() {
+    $('img').click(function(e) {
+      $('button').css({'visibility':'visible'});
+      var url = e.target.getAttribute("src");
+      var currentCat = e.target.parentNode.id;
+      var otherCat;
+      switch(currentCat) {
+        case "photo1":
+          otherCat = $("#photo2 img").attr("src");
+          if($("#photo1 img").hasClass("highlight")) {
+            $(this).removeClass("highlight");
+            testTracker.decrementVote(url);
+          } else {
+            $(this).addClass("highlight");
+            testTracker.incrementVote(url);
+          }
+          if($("#photo2 img").hasClass("highlight")) {
+            $("#photo2 img").removeClass("highlight");
+            testTracker.decrementVote(otherCat);
+          }
+          break;
+        case "photo2":
+          otherCat = $("#photo1 img").attr("src");
+          if($("#photo2 img").hasClass("highlight")) {
+            $(this).removeClass("highlight");
+            testTracker.decrementVote(url);
+          } else {
+            $(this).addClass("highlight");
+            testTracker.incrementVote(url);
+          }
+          if($("#photo1 img").hasClass("highlight")) {
+            $("#photo1 img").removeClass("highlight");
+            testTracker.decrementVote(otherCat);
+          }
+          break;
+      }
+      testTracker.renderChart();
     });
   });
-}
+};
+
+
 
 var testTracker = new Tracker();
 for(var i = 0; i <= 13; i++) {
@@ -91,3 +169,4 @@ for(var i = 0; i <= 13; i++) {
   testTracker.addPhoto(photo);
 };
 testTracker.renderComparison();
+testTracker.renderChart();
