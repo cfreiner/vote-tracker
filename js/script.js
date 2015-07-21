72fb97580ec23d7694d05a562acd4e3cb53d31be
Carl Reiner <creiner1@gmail.com>
2015-07-20 21:02:56 -0700

    Added code comments and README description.

--- js/script.js
+++ js/script.js
@@ -6,25 +6,31 @@ var Tracker = function(){
   this.photos = [];
 }
 
-Tracker.prototype.generateRandom = function(numPhotos) {
-  return Math.floor(Math.random() * numPhotos);
+//Generated a random number between zero and max
+Tracker.prototype.generateRandom = function(max) {
+  return Math.floor(Math.random() * max);
 };
 
+//Picks a random number between zero and the number of photos
+//Deprecated function. To be removed soon.
 Tracker.prototype.pickRandomPhoto = function(){
   return this.generateRandom(this.photos.length);
 };
 
+//Returns an img element for a random photo
 Tracker.prototype.renderRandomPhoto = function() {
-  var random = this.pickRandomPhoto();
+  var random = this.generateRandom(this.photos.length);
   var photo = '<img src="' + this.photos[random][0].url + '" />' + this.photos[random][1];
   return photo;
 };
 
+//Add a photo to the array along with an int for counting votes.
 Tracker.prototype.addPhoto = function(photo) {
   var photoArray = [photo,0];
   this.photos.push(photoArray);
 };
 
+//Display two random photos
 Tracker.prototype.renderComparison = function(){
   var photo1 = this.renderRandomPhoto();
   var photo2 = this.renderRandomPhoto();
@@ -38,6 +44,8 @@ Tracker.prototype.renderComparison = function(){
   highlight();
 };
 
+//Gets the index of a photo in the array with a particular url.
+//Returns false if the photo isn't there.
 Tracker.prototype.findPhotoInArray = function(url) {
   console.log(url);
   for(var i = 0; i < this.photos.length; i++){
@@ -48,11 +56,14 @@ Tracker.prototype.findPhotoInArray = function(url) {
   return false;
 };
 
+//Add a vote for a photo
 Tracker.prototype.incrementVote = function(photo){
   var index = this.findPhotoInArray(photo);
   this.photos[index][1]++;
 };
 
+//Event listener for the button click
+//May be moved to the Tracker constructor later
 $(document).ready(function(){
   $('button').click(function(){
     testTracker.incrementVote($('.highlight').attr('src'));
@@ -61,6 +72,8 @@ $(document).ready(function(){
   });
 });
 
+//Event listener for highlighting the user choice.
+//Has to be called every time new images are drawn to reattach the listener.
 var highlight = function(){
   $(document).ready(function(){
     $('img').click(function(){
@@ -72,7 +85,6 @@ var highlight = function(){
 }
 
 var testTracker = new Tracker();
-testTracker.generateRandom(14);
 for(var i = 0; i <= 13; i++) {
   var photoURL = "img/" + i + ".jpg";
   var photo = new Photo(photoURL);
